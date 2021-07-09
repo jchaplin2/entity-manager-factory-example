a425fa10cfa37d174e272e9b351d4b334edbc735
Jon Chaplin <jchaplin@gmail.com>
2021-07-09 08:49:14 -0400

    Initial check-in

--- resources/schema-it.sql
+++ resources/schema-it.sql
@@ -1,2 +1,4 @@
 create sequence hibernate_sequence start with 1 increment by 1
 create table MODEL (ID bigint not null, VAL varchar(255) not null, primary key (ID))
+create sequence hibernate_sequence start with 1 increment by 1
+create table MODEL (ID bigint not null, VAL varchar(255) not null, primary key (ID))
